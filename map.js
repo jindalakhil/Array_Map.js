7913ef51dbbb96fc685d8a24597212f8393d77a5
jindalakhil <akhiljindal222@gmail.com>
2020-11-10 00:48:46 +0530

    [Akhil]Added: Array , map and Birthday month program

---
+++ map.js
@@ -0,0 +1,19 @@
+var map = new Map();
+    var value_map = new Map();
+    var dice_roll_number = 1;
+    var random = 0;
+    let count = 10;
+    while(map.get(random)!=count){
+    random = Math.floor(Math.random()*10)%6+1;
+    value_map.set(dice_roll_number++,random)
+
+    if(map.has(random))
+        map.set(random, map.get(random)+1);
+    else{
+        map.set(random,1);
+        console.log("key set.");
+    }
+    // console.log("Random dice roll value: " +random);
+    }
+    console.log(value_map)
+    console.log("Dice value having count 10 " + random)
